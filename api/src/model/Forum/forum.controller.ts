e75b61fc01a573ad263dafe75b7296690f0f1476
Samy801172 <abdeljalil.lemkadem@student.efpl.be>
2024-06-13 07:59:33 +0200

    Add and commit all pending changes and untracked files

--- api/src/model/Forum/forum.controller.ts
+++ api/src/model/Forum/forum.controller.ts
@@ -1,17 +1,20 @@
-
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ForumService } from './forum.service';
 import { CreateForumDto } from './create-forum.dto';
 
+// The controller for forum-related endpoints
 @Controller('forum')
 export class ForumController {
+  // Injecting the ForumService
   constructor(private readonly forumService: ForumService) {}
 
+  // Endpoint to create a new forum entry
   @Post()
   async create(@Body() createForumDto: CreateForumDto) {
     return this.forumService.create(createForumDto);
   }
 
+  // Endpoint to retrieve all forum entries
   @Get()
   async findAll() {
     return this.forumService.findAll();
