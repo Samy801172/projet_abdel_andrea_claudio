d6221964d3c2c03a8e6ae8429065235862bb80c8
Samy801172 <abdeljalil.lemkadem@student.efpl.be>
2024-06-25 19:58:39 +0200

    commit frontend

--- api/src/model/Cryptocurrency/dto/create-cryptocurrency.dto.ts
+++ api/src/model/Cryptocurrency/dto/create-cryptocurrency.dto.ts
@@ -1,6 +1,10 @@
 // src/model/Cryptocurrency/dto/create-cryptocurrency.dto.ts
 
+import {ApiProperty} from '@nestjs/swagger';
+
 export class CreateCryptocurrencyDto {
+  @ApiProperty()
   name_crypto: string;
+  @ApiProperty()
   value_crypto: number;
 }
--- api/src/model/Forum/dto/create-forum.dto.ts
+++ api/src/model/Forum/dto/create-forum.dto.ts
@@ -3,23 +3,18 @@ import { IsNotEmpty, IsString, IsDate } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateForumDto {
-  @IsNotEmpty()
-  @IsString()
+
   @ApiProperty()
   name: string | undefined;
 
-  @IsNotEmpty()
-  @IsString()
+
   @ApiProperty()
   description: string | undefined;
 
-  @IsNotEmpty()
-  @IsDate()
+
   @ApiProperty()
   date_message: Date | undefined;
 
-  @IsNotEmpty()
-  @IsDate()
   @ApiProperty()
   hour_message: Date | undefined;
 }
